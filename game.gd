226bf3fc3f396b3372fcd6a0db342dc019885802
Yusuf Kılıçaslan <klcslnyusuf2105@gmail.com>
2026-04-17 21:54:54 +0300

    Add timer and restart flow to collector game

--- game.gd
+++ game.gd
@@ -1,22 +1,41 @@
 extends Node2D
 
 var score := 0
+var time_left := 20
+var game_active := true
+
 var rng := RandomNumberGenerator.new()
 
-@onready var score_label = $CanvasLayer/ScoreLabel
+@onready var score_label = $CanvasLayer/UIBox/ScoreLabel
+@onready var time_label = $CanvasLayer/UIBox/TimeLabel
+@onready var status_label = $CanvasLayer/UIBox/StatusLabel
 @onready var player = $Player
 @onready var collectible = $Collectible
+@onready var game_timer = $GameTimer
 
 func _ready():
 	rng.randomize()
+	start_new_game()
+
+func start_new_game():
+	score = 0
+	time_left = 20
+	game_active = true
+	status_label.text = "Topla!"
 	update_ui()
 	move_collectible()
+	game_timer.start()
 
 func _physics_process(_delta):
-	check_collect()
+	if game_active:
+		check_collect()
+
+	if Input.is_action_just_pressed("ui_accept") and not game_active:
+		start_new_game()
 
 func update_ui():
 	score_label.text = "Skor: %d" % score
+	time_label.text = "Süre: %d" % time_left
 
 func move_collectible():
 	var size = get_viewport_rect().size
@@ -30,3 +49,18 @@ func check_collect():
 		score += 1
 		update_ui()
 		move_collectible()
+
+func _on_game_timer_timeout():
+	if not game_active:
+		return
+
+	time_left -= 1
+	update_ui()
+
+	if time_left <= 0:
+		finish_game()
+
+func finish_game():
+	game_active = false
+	game_timer.stop()
+	status_label.text = "Süre doldu! Enter ile yeniden başlat."
